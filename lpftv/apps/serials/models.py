55f4e60136391124360db9b223570e5a50043361
IvAn <ivan7790@gmail.com>
2010-10-16 23:09:09 +0300

    Create image miniature Cann't auto delete pthotos and miniatures from 'photos'

--- lpftv/apps/serials/models.py
+++ lpftv/apps/serials/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import Image, os
 
 class Serial(models.Model):
     name = models.CharField(max_length = 50, verbose_name = "Serials' name")
@@ -7,8 +8,35 @@ class Serial(models.Model):
     origin_img = models.ImageField(upload_to = "photos") 
     pub_date = models.DateTimeField()
 
+    def save(self): 
+         oldfile = self.origin_img.path
+         models.Model.save(self)
+         print oldfile + " : " + str(os.path.exists(oldfile))
+         if os.path.exists(oldfile):
+             print oldfile + " : " + os.path.exists(oldfile)
+             os.remove(oldfile)
+         oldfile = oldfile[:-4]+ "_small.jpeg"
+         if os.path.exists(oldfile):
+             os.remove(oldfile) 
+         size = 128
+         try:
+             im = Image.open(self.origin_img.path)
+             im.thumbnail((size, size), Image.ANTIALIAS)
+             im.save(self.origin_img.path[:-4] + "_small.jpeg", "jpeg")
+         except IOError:
+            print "IOError: cann't resize image "
+
+    def delete(self, save=True):
+        name = self.origin_img.path[:4] + "_small.jpeg"
+        if os.path.exists(name):
+            os.remove(name)
+        model.Model.delete(self, save)
+
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        ordering = ('-pub_date',)
 
 class Movie(models.Model):
     serial = models.ForeignKey(Serial)
@@ -19,6 +47,9 @@ class Movie(models.Model):
     movie = models.FileField(upload_to = "serials")
     pub_date = models.DateTimeField()
 
+    class Meta:
+        ordering = ('-pub_date',)
+
     def __unicode__(self):
         return self.name
 
@@ -28,9 +59,21 @@ class New(models.Model):
     text = models.TextField(verbose_name = "News")
     pub_date = models.DateTimeField("Publicated date")
 
+    class Meta:
+         ordering = ('-pub_date',)
+
+    def __unicode__(self):
+        return name
+
 class Comment(models.Model):
     author = models.CharField(max_length = 50, verbose_name = "Author")
     text = models.TextField(verbose_name = "Comment")
-    link = models.ForeignKey(Serial or New or Movie)
+    link = models.ForeignKey(Movie)
     pub_date = models.DateTimeField("Comment's date")
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __unicode__(self):
+        return author + ": " +links
 
