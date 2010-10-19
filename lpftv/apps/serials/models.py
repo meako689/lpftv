dd763a4278f3d904aabecad9c170ecd22283d322
IvAn <ivan7790@gmail.com>
2010-10-20 01:03:16 +0300

    Add some docstring

--- lpftv/apps/serials/models.py
+++ lpftv/apps/serials/models.py
@@ -2,30 +2,37 @@ from django.conf import settings
 from django.db import models
 import Image, os
 
+__docformat__ = "restructuredtext"
+
 class CFilm(models.Model):
+    """
+    Base model for 'Movie' and 'Serial 
+    """
     name = models.CharField(max_length = 50, verbose_name = "Serials' name")
     full_describe = models.TextField(verbose_name = "Full describe", blank = True)
     origin_img = models.ImageField(upload_to = "photos", blank = True) 
     last_img = models.ImageField(upload_to = "photos", editable = False, default = "")
     pub_date = models.DateTimeField()
 
-#Url of miniature image
     def get_small_url(self):
+        """Return url to small image"""
         return self.origin_img.url[:self.origin_img.url.rindex('.')]+ \
             ".small"+self.origin_img.url[self.origin_img.url.rindex('.'):]
 
     def get_small_path(self):
+        """Return path in local disk to small image"""
         return self.origin_img.path[:self.origin_img.path.rindex('.')]+ \
             ".small"+self.origin_img.path[self.origin_img.path.rindex('.'):]
 
     def get_last_img_path(self):
+        """This function used only in pre_save()"""
         try:
             return self.last_img.path[:self.last_img.path.rindex('.')]+ \
                 ".small"+self.last_img.path[self.last_img.path.rindex('.'):]
         except: '' 
 
-#Create miniature image
     def save(self):
+        """Saving small image 'size'x'size' in local disk"""
         models.Model.save(self)
         size = 200
         try:
@@ -35,8 +42,9 @@ class CFilm(models.Model):
         except IOError:
             print "Error"
 
-#Delete olds images
+
     def pre_save(self):
+        """Remove old images"""
         try: 
             if os.path.exists(self.get_last_img_path()):
                 os.remove(self.get_last_img_path())
@@ -45,8 +53,8 @@ class CFilm(models.Model):
         except: pass 
         self.last_img = self.origin_img
 
-#Delete images when deleting model 
     def delete(self):
+        """Remove images"""
         try:
             if os.path.exist(self.origin_img.path):
                 os.remove(self.origin_img.path)
@@ -62,6 +70,7 @@ class CFilm(models.Model):
 
 
 def pre_save(sender, **kwargs):
+    """Call 'pre_save()' to all objects what saving"""
     sobject = kwargs['instance']
     try:
         sobject.pre_save()
@@ -69,9 +78,11 @@ def pre_save(sender, **kwargs):
         None
 
 class Serial(CFilm):
+    """Contein serial describe and movie"""
     short_describe = models.TextField(verbose_name = "Short describe")
 
 class Movie(CFilm):
+    """It's only movie"""
     serial = models.ForeignKey(Serial)
     movie = models.FileField(upload_to = "serials")
 
