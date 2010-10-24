4280b5c18c52a2db9aa0d7a58b15b56eca15835f
IvAn <ivan7790@gmail.com>
2010-10-24 22:13:58 +0300

    Change 'dateil' => 'detail'

--- lpftv/apps/serials/urls.py
+++ lpftv/apps/serials/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('apps.serials.views',
     (r'^$', 'index'),
     (r'^serial/$','show_serial'),
     (r'^news/$', 'show_news'),
-    url(r'^serial/(?P<s_id>\d+)/$', 'serial_dateil', name="serial_dateil"),
-    url(r'^news/(?P<n_id>\d+)/$', 'news_dateil', name="news_dateil"),
+    url(r'^serial/(?P<s_id>\d+)/$', 'serial_detail', name="serial_detail"),
+    url(r'^news/(?P<n_id>\d+)/$', 'news_detail', name="news_detail"),
 )
 
