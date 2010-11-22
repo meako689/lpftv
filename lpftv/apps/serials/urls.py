1c82bb60f70514d83f20d00b4ccf6634cd06f96b
Meako <meako689@gmail.com>
2010-11-23 00:27:09 +0200

    Changed structure of serials app.
    Migration needed

--- lpftv/apps/serials/urls.py
+++ lpftv/apps/serials/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('apps.serials.views',
-    url(r'^$', 'index', name = 'index'),
     url(r'^find/$', 'find', name='find'),
     url(r'^series/$','show_serial', name = 'serials_index'),
-    url(r'^news/$', 'show_news', name = 'news_index'),
-    url(r'^series/(?P<s_id>\d+)/$', 'serial_detail', name="serial_detail"),
-    url(r'^series/(?P<s_id>\d+)/(?P<e_id>\d+)/$', 'episode_detail', name="episode_detail"),
+    url(r'^$', 'show_news', name = 'news_index'),
+    url(r'^series/(?P<slug>\w+)/$', 'serial_detail', name="serial_detail"),
+    url(r'^series/(?P<slug>\w+)/(?P<e_id>\d+)/$', 'episode_detail', name="episode_detail"),
     url(r'^news/(?P<n_id>\d+)/$', 'news_detail', name="news_detail"),
 )
 
--- lpftv/apps/serials/views.py
+++ lpftv/apps/serials/views.py
@@ -1,14 +1,9 @@
 from markdown import markdown
-from django.views.generic.list_detail import object_detail
+from django.views.generic.list_detail import object_detail, object_list
 from django.shortcuts import render_to_response, get_object_or_404
 from apps.serials.models import Serial, Episode, News
 from django.conf import settings
 from django.template import RequestContext
-
-def index(request):
-     serials = Serial.objects.all()[:5]
-     news = News.objects.all() 
-     return render_to_response("serials/index.html", {'news': news, 'serials': serials}, context_instance=RequestContext(request))
 
 def show_news(request):
      news = News.objects.all()
@@ -18,15 +13,14 @@ def show_serial(request):
      serials = Serial.objects.all()
      return render_to_response("serials/serials.html", {'serials': serials}, context_instance=RequestContext(request))
 
-def serial_detail(request, s_id):
-     serials = get_object_or_404(Serial, id = s_id)
+def serial_detail(request, slug):
+     serials = get_object_or_404(Serial, slug = slug)
      episodes = Episode.objects.filter(serial = serials)
      return render_to_response("serials/serial_detail.html", {'serial': serials, 'episodes': episodes, 'next': serials.get_absolute_url()}, context_instance=RequestContext(request))
 
-def episode_detail(request ,s_id, e_id):
+def episode_detail(request ,slug, e_id):
     return object_detail(request,
         queryset = Episode.objects.all(),
-        object_id = e_id,
         template_name = "serials/episode_detail.html"
         )
 
