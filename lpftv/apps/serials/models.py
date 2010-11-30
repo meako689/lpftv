c9bfcdb90e01f197ce170c75f3dbd1ef7afe1919
IvAn <ivan7790@gmail.com>
2010-11-30 17:20:16 +0200

    Images such which must be

--- lpftv/apps/serials/models.py
+++ lpftv/apps/serials/models.py
@@ -13,28 +13,28 @@ class CFilm(models.Model):
     """
     name = models.CharField(max_length = 50, verbose_name = "name")
     full_description = models.TextField(verbose_name = "Full description", blank = True)
-    origin_img = models.ImageField(upload_to = "photos", blank = True) 
-    last_img = models.ImageField(upload_to = "photos", editable = False, blank = True, default = "")
+    origin_img = models.ImageField(upload_to = "photos/", blank = True) 
+    last_img = models.ImageField(upload_to = "photos/", editable = False, blank = True, default = "")
     pub_date = models.DateTimeField(default = datetime.now)
 
-    def get_thumb_url(self):
+    def get_thumb_url(self, exp = ".small"):
         """Return url to small image(if not small image return default)"""
         if self.origin_img:
             return self.origin_img.url[:self.origin_img.url.rindex('.')]+ \
-                ".small"+self.origin_img.url[self.origin_img.url.rindex('.'):]
+                exp+self.origin_img.url[self.origin_img.url.rindex('.'):]
         else:
             return settings.IMAGE_DEFAULT
 
-    def get_thumb_path(self):
+    def get_thumb_path(self, exp = ".small"):
         """Return path in local disk to small image"""
         return self.origin_img.path[:self.origin_img.path.rindex('.')]+ \
-            ".small"+self.origin_img.path[self.origin_img.path.rindex('.'):]
+            exp+self.origin_img.path[self.origin_img.path.rindex('.'):]
 
-    def get_last_img_path(self):
+    def get_last_img_path(self, exp = ".small"):
         """This function used only in pre_save()"""
         if self.last_img:
             return self.last_img.path[:self.last_img.path.rindex('.')]+ \
-                ".small"+self.last_img.path[self.last_img.path.rindex('.'):]
+                exp+self.last_img.path[self.last_img.path.rindex('.'):]
         else: "" 
 
     def get_image_url(self):
@@ -44,37 +44,41 @@ class CFilm(models.Model):
         else:
             return settings.IMAGE_DEFAULT
 
-    def save(self):
-        """Saving small image 'size'x'size' in local disk"""
-        super(CFilm, self).save()
-        size = settings.IMAGE_XY
-        if self.origin_img:
+    def save_thumb(self, size, crop, im_path, tm_path):
+        if im_path:
             try:
-                im = Image.open(self.origin_img.path)
-                #Code for crop image
-                offset = im.size[0] - im.size[1]
-                offset /= 2
-                if offset > 0:
-                    box = (offset, 0, im.size[0]-offset, im.size[1])
-                if offset < 0:
-                    offset *= -1
-                    box = (0, offset, im.size[0], im.size[1]-offset)
-                im = im.crop(box)
-                #End code 
+                im = Image.open(im_path)
+                if crop:
+                    offset = im.size[0] - im.size[1]
+                    offset /= 2
+                    if offset > 0:
+                        box = (offset, 0, im.size[0]-offset, im.size[1])
+                    if offset < 0:
+                        offset *= -1
+                        box = (0, offset, im.size[0], im.size[1]-offset)
+                    im = im.crop(box)
                 im.thumbnail((size, size), Image.ANTIALIAS)
-                im.save(self.get_thumb_path(), "jpeg") 
+                im.save(tm_path, "jpeg") 
             except IOError:
                 print "Cann't get small image!"
 
+    def save(self, crop = False, size = settings.IMAGE_XY):
+        """Saving small image 'size'x'size' in local disk"""
+        super(CFilm, self).save()
+        if self.origin_img:
+            im_path = self.origin_img.path
+            tm_path = self.get_thumb_path()
+            self.save_thumb(size, crop, im_path, tm_path)
 
 
     def delete(self):
         """Remove images and remove record in database"""
         try:
-            if os.path.exist(self.origin_img.path):
-                os.remove(self.origin_img.path)
-            if os.path.exists(self.get_thumb_path()):
-                os.remove(self.get_thumb_path())
+            if self.origin_img:
+                if os.path.exists(self.origin_img.path):
+                    os.remove(self.origin_img.path)
+                if os.path.exists(self.get_thumb_path()):
+                    os.remove(self.get_thumb_path())
         except IOError: 
             print "Cann't delete file"
 
@@ -102,7 +106,16 @@ def remove_imgs(sender, instance, *args, **kwargs):
 class Serial(CFilm):
     """Contains serial description and movie"""
     short_description = models.TextField(verbose_name = "Short description")
-
+    
+    def save(self):
+        super(Serial, self).save(crop = False)
+        if self.origin_img and os.path.exists(self.origin_img.path): 
+             im_path = self.origin_img.path
+             tm_path = self.get_thumb_path(exp = ".crop")
+             crop = True
+             size = settings.IMAGE_XY
+             self.save_thumb(size, crop, im_path, tm_path)
+               
     def get_absolute_url(self):
         return reverse('serial_detail', args=[self.id])
 
@@ -127,7 +140,7 @@ class Episode(CFilm):
             return self.origin_img.url[:self.origin_img.url.rindex('.')]+ \
                 ".small"+self.origin_img.url[self.origin_img.url.rindex('.'):]
         else:
-            return self.serial.get_thumb_url()
+            return self.serial.get_thumb_url(".crop")
 
     def get_absolute_url(self):
         return reverse('episode_detail', args=[self.serial.id, self.id])
@@ -164,4 +177,3 @@ class RImage(models.Model):
 
 models.signals.pre_save.connect(remove_imgs, sender = Episode)
 models.signals.pre_save.connect(remove_imgs, sender = Serial )
-
