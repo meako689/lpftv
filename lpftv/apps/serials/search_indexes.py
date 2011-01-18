4592236d0366b4b66b9497d16a9537782a8dda1c
IvAn <ivan7790@gmail.com>
2011-01-18 15:51:34 +0200

    The search is work. Search is in a News, Serial, Episode in all text fileds

--- lpftv/apps/serials/search_indexes.py
+++ lpftv/apps/serials/search_indexes.py
@@ -1,15 +1,33 @@
 import datetime
 from haystack.indexes import *
 from haystack import site
-from apps.serials.models import Serial
+from apps.serials.models import Serial, Episode, News
 
 
-class SerialIndex(SearchIndex):
+class EpisodeIndex(SearchIndex):
     name = CharField(document=True, use_template=True)
+    full_description = CharField()
     pub_date = DateTimeField(model_attr='pub_date')
+
+    def get_queryset(self):
+        return Episode.objects.all()
+
+class SerialIndex(EpisodeIndex):
+    short_description = CharField()
 
     def get_queryset(self):
         return Serial.objects.all()
 
+class NewsIndex(SearchIndex):
+    name = CharField(document=True, use_template=True)
+    short_description = CharField()
+    full_description = CharField()
+    pub_date = DateTimeField(model_attr="pub_date")
 
+    def get_queryset(self):
+        return News.objects.all()
+
+
+site.register(Episode, EpisodeIndex)
 site.register(Serial, SerialIndex)
+site.register(News, NewsIndex)
